ead492834f57cf33f2b1dba3eb79f6345ad24ad0
Timbar21 <timbar21pb@gmail.com>
2016-02-07 15:25:55 -0500

    first init

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Tim Barnard on 2/7/16.
+//  Copyright © 2016 Tim Barnard. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
